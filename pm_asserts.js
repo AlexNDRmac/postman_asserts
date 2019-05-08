5c863de172ea4880d6e9387416849a2dd7ae471e
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-05-08 17:58:00 +0300

    add basic project frame

---
+++ pm_asserts.js
@@ -0,0 +1,42 @@
+// Reusable Functions for Postman Asserts
+let functions = function loadAsserts() {
+    let asserts = {};
+
+    asserts.isJsonResponse = function isJsonResponse() {
+        pm.test('Response should have JSON Body', function () {
+            pm.response.to.have.jsonBody();
+        });
+    };
+
+    asserts.isResponseStatus = function isResponseStatus(statusCode) {
+        pm.test('Status code is ' + statusCode, function () {
+            pm.response.to.have.status(statusCode);
+        });
+    };
+
+    asserts.validateJsonSchema = function validateJsonSchema(schema) {
+        tests["Response Data is Valid"] = tv4.validate(pm.response.json(), schema);
+
+        if (tv4.error !== null) {
+           tests["Validation ERROR => " + tv4.error.message] = "";
+        }
+    };
+
+    asserts.isEquals = function isEquals(expected, actual, message) {
+        message = message || 'Expected value is Equals to actual';
+
+        pm.test(message, function () {
+            pm.expect(expected).to.eql(actual);
+        });
+    };
+
+    return asserts;
+} + '; loadAsserts();';
+
+let postmanAsserts = JSON.stringify(functions);
+
+if (!pm.globals.has('postmanAsserts') || pm.globals.get('postmanAsserts') !== postmanAsserts) {
+    pm.globals.set('postmanAsserts', postmanAsserts);
+
+    tests['Set up Postman Asserts to Globals'] = pm.globals.has('postmanAsserts');
+}
