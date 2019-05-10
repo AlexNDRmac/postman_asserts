87a76d374ccbab412bf352f1d5df7a3a3fccc0ca
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-05-10 14:36:28 +0300

    provide simply func instead of complex object

--- pm_asserts.js
+++ pm_asserts.js
@@ -1,33 +1,30 @@
-var functions = function loadAsserts() {
-    var asserts = {};
+/**
+ * This script defines global variables to provide Asserts for Postman unit test case Runner.
+ */
 
-    asserts.isJsonResponse = function isJsonResponse() {
-        pm.test('Response should have JSON Body', function () {
-            pm.response.to.have.jsonBody();
-        });
-    };
+function isJsonResponse() {
+    pm.test('Response should have JSON Body', function () {
+        pm.response.to.have.jsonBody();
+    });
+}
 
-    asserts.isResponseStatus = function isResponseStatus(statusCode) {
-        pm.test('Status code is ' + statusCode, function () {
-            pm.response.to.have.status(statusCode);
-        });
-    };
+function isResponseStatus(statusCode) {
+    pm.test('Status code is ' + statusCode, function () {
+        pm.response.to.have.status(statusCode);
+    });
+}
 
-    asserts.validateJsonSchema = function validateJsonSchema(schema) {
-        tests["Response Data is Valid"] = tv4.validate(pm.response.json(), schema);
+function validateJsonSchema(schema) {
+    tests["Response Data is Valid"] = tv4.validate(pm.response.json(), schema);
 
-        if (tv4.error !== null) {
-           tests["Validation ERROR => " + tv4.error.message] = "";
-        }
-    };
+    if (tv4.error !== null) {
+        tests["Validation ERROR => " + tv4.error.message] = "";
+    }
+}
+function isEquals(expected, actual, message) {
+    message = message || 'Expected value is Equals to actual';
 
-    asserts.isEquals = function isEquals(expected, actual, message) {
-        message = message || 'Expected value is Equals to actual';
-
-        pm.test(message, function () {
-            pm.expect(expected).to.eql(actual);
-        });
-    };
-
-    return asserts;
-};
+    pm.test(message, function () {
+        pm.expect(expected).to.eql(actual);
+    });
+}
